770cfaf3609c7c6db97d5588781b2433f30fcc43
tsnorri <tsnorri@iki.fi>
2015-08-28 15:45:41 +0300

    Completed exercise 51

--- public/app/controllers/show_topic_controller.js
+++ public/app/controllers/show_topic_controller.js
@@ -1,3 +1,7 @@
-FoorumApp.controller('ShowTopicController', function($scope, $routeParams, $location, Api){
-  // Toteuta kontrolleri tähän
+FoorumApp.controller('ShowTopicController', function($scope, $routeParams, $location, Api) {
+	Api.getTopic($routeParams.id).success(function(data, status, headers, config) {
+		$scope.topic = data;
+	}).error(function(data, status, headers, config) {
+		alert(":( " + status + " " + data);
+	});
 });
--- public/app/controllers/topics_list_controller.js
+++ public/app/controllers/topics_list_controller.js
@@ -1,3 +1,18 @@
 FoorumApp.controller('TopicsListController', function($scope, $location, Api){
-  // Toteuta kontrolleri tähän
+	$scope.newTopic = {};
+	
+	Api.getTopics().success(function(data, status, headers, config) {
+		$scope.topics = data;
+	}).error(function(data, status, headers, config) {
+		alert(":( " + status + " " + data);
+	});
+	
+	$scope.addTopic = function() {
+		Api.addTopic($scope.newTopic).success(function(data, status, headers, config) {
+			//$scope.topics.push(data);
+			$location.path("/topics/" + data.id);
+		}).error(function(data, status, headers, config) {
+			alert(":( " + status + " " + data);
+		});
+	};
 });
