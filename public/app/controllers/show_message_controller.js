ae88e4668369d58fab18d9ee5466e06cbf709cce
tsnorri <tsnorri@iki.fi>
2015-08-28 16:35:38 +0300

    Completed exercise 53

--- public/app/controllers/show_message_controller.js
+++ public/app/controllers/show_message_controller.js
@@ -1,3 +1,18 @@
 FoorumApp.controller('ShowMessageController', function($scope, $routeParams, Api){
-  // Toteuta kontrolleri tähän
+	$scope.newReply = {};
+
+	Api.getMessage($routeParams.id).success(function(data, status, headers, config) {
+		$scope.message = data;
+	}).error(function(data, status, headers, config) {
+		alert(":( " + status + " " + data);
+	});
+	
+	$scope.sendReply = function() {
+		Api.addReply($scope.newReply, $scope.message.id).success(function(data, status, headers, config) {
+			$scope.message.Replies.push(data);
+			$scope.newReply = {};
+		}).error(function(data, status, headers, config) {
+			alert(":( " + status + " " + data);
+		});
+	};
 });
--- public/app/controllers/show_topic_controller.js
+++ public/app/controllers/show_topic_controller.js
@@ -1,7 +1,18 @@
 FoorumApp.controller('ShowTopicController', function($scope, $routeParams, $location, Api) {
+	$scope.newMessage = {};
+
 	Api.getTopic($routeParams.id).success(function(data, status, headers, config) {
 		$scope.topic = data;
 	}).error(function(data, status, headers, config) {
 		alert(":( " + status + " " + data);
 	});
+	
+	$scope.sendMessage = function() {
+		Api.addMessage($scope.newMessage, $scope.topic.id).success(function(data, status, headers, config) {
+			//$scope.topic.Messages.push(data);
+			$location.path("/messages/" + data.id);
+		}).error(function(data, status, headers, config) {
+			alert(":( " + status + " " + data);
+		});
+	};
 });
